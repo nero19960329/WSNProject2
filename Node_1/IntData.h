392f1cdcc0207a8d8ac86249dd88c97856293334
nero19960329 <wangzhao2013tsinghua@gmail.com>
2015-12-26 19:08:56 +0800

    hehe

--- Node_1/IntData.h
+++ Node_1/IntData.h
@@ -11,6 +11,10 @@ typedef nx_struct intdata_msg_t {
 	nx_uint32_t random_integer;
 }intdata_msg_t;
 
+typedef nx_struct filldata_msg_t {
+	nx_uint16_t sequence_number;
+}filldata_msg_t;
+
 enum {
   AM_MSG = 0,
 };
